09da2495f5152a8eb0cecc1d2a6510b53ef1c05b
Matt Talbot <mtalbot@vertigo.com>
2014-03-17 20:36:12 -0700

    added 404 handling

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require("express"),
     app     = express(),
     port    = process.env.PORT || 1337;
-   
+
 app.use(express.static(__dirname + '/build'));
 
+app.use(function(req, res, next){
+  console.log(__dirname);
+  res.status(404).sendfile(__dirname + '/404.html');
+});
+
 app.listen(port);
+console.log("web server started on port " + port);
