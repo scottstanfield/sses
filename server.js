059c3e38d0eb94ea806f1e0ddad8740e1d11aeea
Matt Talbot <mtalbot@vertigo.com>
2014-03-17 21:41:30 -0700

    server js changes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express"),
-    app     = express(),
+    server  = express(),
     port    = process.env.PORT || 1337;
 
-app.use(express.static(__dirname + '/build'));
+server.use(express.static(__dirname + '/build'));
 
 // 404 Error Handling
-app.use(function(req, res, next){
+server.use(function(req, res, next){
   res.status(404).sendfile(__dirname + '/404.html');
 });
 
-app.listen(port);
-console.log("web server started on port " + port);
+
+server.listen(port, function(){
+  console.log("web server started on port " + port);
+});
